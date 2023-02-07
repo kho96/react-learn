4d6996b356b5c60dfedce54d8ad03f48315d0144
kho96 <k____ho__@naver.com>
2023-02-07 19:16:27 +0900

    use useEffect function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+/*
 import Button from "./Button"; //Button.js를 가져온다.
 import styles from "./App.module.css";
+*/
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyONce = () => {
+    console.log("i run oly once.");
+  }
+  useEffect(iRunOnlyONce, []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!</h1>
-      <Button text={"Countinue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
