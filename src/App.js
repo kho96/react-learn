4524491b9cf94996bfe51755cc1a039e384a4439
kho96 <k____ho__@naver.com>
2023-02-08 13:46:51 +0900

    useEffect, argument and practice

--- src/App.js
+++ src/App.js
@@ -6,14 +6,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => {
+      setKeyword(event.target.value);
+  }
   console.log("i run all the time");
   const iRunOnlyONce = () => {
     console.log("i run oly once.");
   }
-  useEffect(iRunOnlyONce, []);
+  useEffect(iRunOnlyONce, []); // 화면이 실행되고 최초 한 번 실행된다.
+  // useEffect function은 rerendering 되더라도 실행되지 않음
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search For " ,keyword);
+    }
+  }, [keyword]); //keyword가 변화할 때 만 실행된다.
+  
   return (
     <div>
+      <input value={keyword} onChange={onChange}
+        type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
